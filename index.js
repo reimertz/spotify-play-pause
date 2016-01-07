f805f1d7c7b9c4a9f98a4e88ae94fbd368ec1b0f
Pierre Reimertz <pierre@pierrereimertz.com>
2016-01-07 18:28:09 +0100

    express -> johnny-five

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-var express = require('express');
-var spotify = require('spotify-node-applescript');
-var app = express();
-
-app.set('views', './')
-app.set('view engine', 'jade');
-
-app.get('/play', function (req, res) {
-  spotify.play(function() {
-    console.log('played song');
-  });
-  res.render('index.jade', { play: true});
-});
-
-app.get('/pause', function (req, res) {
-  spotify.pause(function() {
-    console.log('paused song');
-  });
-  res.render('index.jade', { pause: true});
-});
+var spotify = require('spotify-node-applescript'),
+    five = require("johnny-five"),
+    isPause  = false;
 
 
-app.get('/', function (req, res) {
-  res.render('index.jade', { pause: true});
-});
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+five.Board().on("ready", function() {
+  bumper = new five.Button(7);
 
-  console.log('play-pause app listening at http://%s:%s', host, port);
+  bumper.on("hit", function() {
+    var method = (shouldPause) ? 'play' : 'pause';
+    isPause = !isPause;
+
+    spotify[method](function() {
+      console.log(method + 'ed song');
+    });
+  })
 });
--- readme.md
+++ readme.md
@@ -6,6 +6,12 @@ npm install
 ```
 
 ##run
+###follow this schematic
+https://github.com/rwaldron/johnny-five/blob/master/docs/button-bumper.md
+
+###then
+
 ```bash
+node index.js
 ```
-then open localhost:3000
+
